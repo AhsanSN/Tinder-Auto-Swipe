0032c3b6d5afec8459f53f4b31ec88bff8bb53c2
Ahsan Ahmed <snahmed1998@gmail.com>
2020-03-30 15:51:01 +0500

    script ready

--- tinder_bot.py
+++ tinder_bot.py
@@ -1,73 +1,78 @@
 from selenium import webdriver
 from time import sleep
+import random
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class TinderBot():
+    #variables
+    swipesPerMinute = 60
+    chromeDriver = ".\chromedriver_win32\\chromedriver.exe"
+    websiteLink = "https://tinder.com/"
+    likeXpath = '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button'
+    dislikeXpath = '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[2]/button'
+    closePopup = '//*[@id="modal-manager"]/div/div/div[2]/button[2]'
+    closeMatch = '//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a'
+    wait = ""
+    
     def __init__(self):
-        self.driver = webdriver.Chrome('.\chromedriver_win32\\chromedriver.exe')
+        self.driver = webdriver.Chrome(self.chromeDriver)
+
+    def waitForLogin(self, desiredUrl):
+        self.wait.until(
+            lambda driverwaiter: driverwaiter.current_url == desiredUrl)
+        print("Login Successfull. Activating Auto-swipe in 20 seconds. Please Wait...")
+        sleep(20)
+        self.auto_swipe()
 
     def open_website(self):
-        self.driver.get("https://tinder.com/app/recs")
+        self.driver.get(self.websiteLink)
+        self.wait = WebDriverWait(self.driver, 45) #45 seconds to login
+        self.waitForLogin("https://tinder.com/app/recs")
         sleep(2)
-
-    def login(self):
-        self.driver.get('https://tinder.com')
-
-        sleep(2)
-
-        fb_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
-        fb_btn.click()
-
-        # switch to login popup
-        base_window = self.driver.window_handles[0]
-        self.driver.switch_to_window(self.driver.window_handles[1])
-
-        email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
-        email_in.send_keys(username)
-
-        pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
-        pw_in.send_keys(password)
-
-        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
-        login_btn.click()
-
-        self.driver.switch_to_window(base_window)
-
-        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-        popup_1.click()
-
-        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-        popup_2.click()
 
     def like(self):
-        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
+        like_btn = self.driver.find_element_by_xpath(self.likeXpath)
         like_btn.click()
 
     def dislike(self):
-        dislike_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[1]')
+        dislike_btn = self.driver.find_element_by_xpath(self.dislikeXpath)
         dislike_btn.click()
 
     def auto_swipe(self):
-        itr=0
-        while (itr<10):
-            itr +=1
-            sleep(0.5)
+        while True:
+            sleep(60/self.swipesPerMinute)
             try:
-                self.like()
-            except Exception:
+                # 80 to 20 percentage
+                randNumber = random.randint(0, 100)
+                if(randNumber<80):                
+                    self.like()
+                else:
+                    self.dislike()
+            except Exception: #random popups handeling
                 try:
                     self.close_popup()
                 except Exception:
                     self.close_match()
 
     def close_popup(self):
-        popup_3 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+        popup_3 = self.driver.find_element_by_xpath(self.closePopup)
         popup_3.click()
 
     def close_match(self):
-        match_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        match_popup = self.driver.find_element_by_xpath(self.closeMatch)
         match_popup.click()
 
+    
 bot = TinderBot()
-#bot.login()
 bot.open_website()
-bot.auto_swipe()
+bot.maximize_window()
+
+
+'''
+Credits: Written by Syed Ahsan Ahmed
+Fiverr: fiverr.com/ahsanahmedsn
+'''
