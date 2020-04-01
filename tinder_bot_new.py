2250c2e34142c6c0913a39b709b857cb99cf518c
Ahsan Ahmed <snahmed1998@gmail.com>
2020-04-01 19:08:28 +0500

    bugs fixed

--- tinder_bot_new.py
+++ tinder_bot_new.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class TinderBot():
     #variables
-    swipesPerMinute = 60
+    swipesPerMinute = 90
     #chromeDriver = ".\chromedriver_win32\\chromedriver.exe" #ahsan fiverr path
     chromeDriver = "/usr/local/bin/chromedriver"
     websiteLink = "https://tinder.com/"
@@ -24,9 +24,10 @@ class TinderBot():
 
     def isPageLoaded(self):
         try:
-            self.driver.find_element_by_xpath(self.closeMatch)
+            self.driver.find_element_by_xpath(self.likeXpath)
             return True
         except Exception:
+            #print("False")
             return False
         return False
 
@@ -40,7 +41,7 @@ class TinderBot():
     def open_website(self):
         self.driver.get(self.websiteLink)
         print("reload fn test:" , self.isPageLoaded())
-        self.wait = WebDriverWait(self.driver, 90) #90 seconds to login
+        self.wait = WebDriverWait(self.driver, 150) #150 seconds to login
         self.waitForLogin("https://tinder.com/app/recs")
         sleep(2)
 
@@ -55,6 +56,7 @@ class TinderBot():
     def auto_swipe(self):
         while True:
             if(self.isPageLoaded()==False):
+                print("stopped for 30 seconds")
                 sleep(30)
             sleep(60/self.swipesPerMinute)
             try:
@@ -66,9 +68,17 @@ class TinderBot():
                     self.dislike()
             except Exception: #random popups handeling
                 try:
+                    print("close popup")
                     self.close_popup()
                 except Exception:
-                    self.close_match()
+                    print("close match")
+                    if(self.isPageLoaded()==False):
+                        print("stopped for 30 seconds")
+                        sleep(30)
+                    try:
+                        self.close_match()
+                    except Exception:
+                        print("back to old loop")
 
     def close_popup(self):
         popup_3 = self.driver.find_element_by_xpath(self.closePopup)
